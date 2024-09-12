e7133b28331a1c668aaf0837e1639c26f7db1560
Matthew Beckham <beckham@Matthews-MacBook-Pro.local>
2024-09-12 13:05:29 -0500

    Updates

--- assets/status-tap-legacy-jETeBCsu.js
+++ assets/status-tap-legacy-jETeBCsu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C-FA3qb2.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-Cu-wAIwr.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
